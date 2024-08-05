c2130dd454e968ecb7799fe47ad710ebb727e448
Minseok Lee <minseok30086@gmail.com>
2024-08-06 02:25:00 +0900

    Add UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Header from './components/Header.jsx';
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
 
 function App() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
-export default App
+export default App;
